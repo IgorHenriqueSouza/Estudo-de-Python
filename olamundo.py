569ba25a252aacb51c75adffe90280c3d7920d0f
IgorHenriqueSouza <br.igorhenrique@gmail.com>
2023-02-15 16:31:24 -0300

    Estudando Python

---
+++ olamundo.py
@@ -0,0 +1,37 @@
+# printar a mensagem na tela
+print("Olá Mundo")
+#------------------------------------------------------
+# Realizar uma operação matematica
+print(2+2)
+print(5*5)
+#------------------------------------------------------
+# Criando Variaveis
+Minha_variavel ="Ola Mundo"
+print(Minha_variavel)
+print(Minha_variavel)
+
+var1 = 1 
+var2 = 1.1
+var3 = "Ola Mundo de novo"
+var4 = True
+var4 = False
+
+print(var1 + var2 , var3 )
+#------------------------------------------------------
+#if e else
+var1 = 1 
+var2 = 1.1
+var4 = True
+var4 = False
+if var1 + var2 == 2.1:
+        print("Verdadeiro")
+else:   print("falso")
+#------------------------------------------------------
+# Operadores relacionais
+# == | Igual
+# != | Diferente
+# >  | maior
+# <  | menor 
+# >= | Maior ou igual
+# <= | Menor ou igual
+
